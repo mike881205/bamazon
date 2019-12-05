9f2061e986ee954da4aa07d8f8869468eebcc315
mike881205 <mike881205@gmail.com>
2019-12-04 21:21:39 -0500

    Added code for 'welcome' menu: inquirer prompt & main switch statement

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,75 @@
 const mysql = require("mysql")
 const inquirer = require("inquirer")
 
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "StirFry16!",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+
+    start();
+});
+
+function start() {
+
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+
+        console.log("")
+        console.log("Welcome to Bamazon!")
+        console.log("")
+        inquirer
+            .prompt({
+                name: "dpt_list",
+                type: "rawlist",
+                choices: ["Electronics", "Hardware", "Games/Toys", "Sporting Goods", "Exit"],
+                // choices: function () {
+                //     let productArray = [];
+                //     for (let i = 0; i < results.length; i++) {
+                //         productArray.push(results[i].department);
+                //     }
+                //     return productArray
+                // },
+                message: "Please select a department"
+            })
+            .then(function (dept) {
+                let department = dept.dpt_list
+                switch (department) {
+                    case "Electronics":
+                        // electronics();
+                        console.log("Under Construction")
+                        connection.end();
+                        break;
+                    case "Hardware":
+                        // hardware();
+                        console.log("Under Construction")
+                        connection.end();
+                        break;
+                    case "Games/Toys":
+                        // gamesToys();
+                        console.log("Under Construction")
+                        connection.end();
+                        break;
+                    case "Exit":
+                        console.log("Thank You for Coming! See You Soon!")
+                        connection.end();
+                        break;
+
+                }
+
+            })
+
+    })
+
+}
+
+
---
+++ mysqlSeed/bamazon_db.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products(
+  id INT NOT NULL AUTO_INCREMENT,
+  product VARCHAR(100) NOT NULL,
+  department VARCHAR(45) NOT NULL,
+  price DECIMAL(10,2) NULL,
+  stock INT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (product, department, price, stock)
+VALUES ('72" Flat Screen TV', "Electronics", 600.00, 6), ("VR Headset", "Electronics", 400.00, 9), ("Wireless Bluetooth Earbuds", "Electronics", 200.00, 12);
+
+INSERT INTO products (product, department, price, stock)
+VALUES ("Cordless Power Drill", "Hardware", 100.00, 8), ("Ratchet Set", "Hardware", 50.00, 12), ("Table Saw", "Hardware", 800.00, 4);
+
+INSERT INTO products (product, department, price, stock)
+VALUES ("'Monopolize' Board Game", "Games/Toys", 15.00, 20), ("'Bug-Man' Action Figure", "Games/Toys", 10.00, 30), ("'Space Battles' Starfighter Model Kit", "Games/Toys", 25.00, 40);
+
+INSERT INTO products (product, department, price, stock)
+VALUES ("Mountain Bike", "Sporting Goods", 300.00, 5), ("Hockey Goalie Mask", "Sporting Goods", 170.00, 7), ("Golf Club Bag", "Sporting Goods", 100.00, 9);
